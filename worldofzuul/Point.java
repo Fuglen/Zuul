8a02fff35ac5a65e55f9c3dcc089f7b4570fd282
Kasper Sørensen <kaspersorensen1988@gmail.com>
2020-11-03 09:44:50 +0100

    Add files via upload

--- worldofzuul/Point.java
+++ worldofzuul/Point.java
@@ -12,11 +12,11 @@ public class Point {
         return point;
     }
 
-    public void addPoint(int points){
-        this.point += getPoint();
+    public void addPoint(int point){
+        this.point = getPoint() + point;
     }
 
-    public void removePoint(int points){
-        this.point -= getPoint();
+    public void removePoint(int point){
+        this.point = getPoint() - point;
     }
 }

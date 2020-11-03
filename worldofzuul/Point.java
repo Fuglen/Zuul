87f292b3e2601065117639ed633f3ef0aa782a50
Kasper Sørensen <kaspersorensen1988@gmail.com>
2020-11-03 09:39:42 +0100

    Add files via upload

--- worldofzuul/Point.java
+++ worldofzuul/Point.java
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-package worldofzuul;
-
-public class Point {
-
-    private int point;
-
-    public Point(){
-
-    }
-
-    public int getPoint() {
-        return point;
-    }
-
-    public void addPoint(int points){
-        this.point += getPoint();
-    }
-
-    public void removePoint(int points){
-        this.point -= getPoint();
-    }
-}
-=======
 package worldofzuul;
 
 public class Point {
@@ -44,4 +20,3 @@ public class Point {
         this.point -= getPoint();
     }
 }
->>>>>>> Stashed changes

d9c84e528f661dc6cdcc9cb1872017e3a318a4a6
NickJ20 <nickj20@student.sdu.dk>
2020-11-24 10:22:21 +0100

    ...

--- src/sample/HomeController.java
+++ src/sample/HomeController.java
@@ -2,22 +2,10 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class HomeController implements Initializable {
+public class HomeController {
     @FXML
     private void EndGame () {
         Platform.exit();
-    }
-    @FXML
-    private void goMain() {
-
-    }
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -2,14 +2,11 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class MainController implements Initializable {
+public class MainController {
     @FXML
     private void EndGame () {
         Platform.exit();
@@ -18,9 +15,5 @@ public class MainController implements Initializable {
     private void goMain(ActionEvent event) throws IOException {
         /*Stage homeStage = FXMLLoader.load(getClass().getResource("home.fxml"));
         homeStage.show();*/
-    }
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }

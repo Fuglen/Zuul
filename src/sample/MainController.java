97bf478afa45e93329a53ac33bcb10e9085d5343
NickJ20 <nickj20@student.sdu.dk>
2020-11-24 10:14:46 +0100

    ...

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -2,8 +2,12 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,7 +16,11 @@ public class MainController implements Initializable {
     private void EndGame () {
         Platform.exit();
     }
-
+    @FXML
+    private void goMain(ActionEvent event) throws IOException {
+        Stage homeStage = FXMLLoader.load(getClass().getResource("home.fxml"));
+        homeStage.show();
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 

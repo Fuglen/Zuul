1e3de3441ce3531d395b966c16e13d5760339e0f
NickJ20 <nickj20@student.sdu.dk>
2020-11-24 10:24:48 +0100

    ...

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -2,6 +2,8 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
@@ -13,7 +15,7 @@ public class MainController {
     }
     @FXML
     private void goMain(ActionEvent event) throws IOException {
-        /*Stage homeStage = FXMLLoader.load(getClass().getResource("home.fxml"));
-        homeStage.show();*/
+        Stage homeStage = FXMLLoader.load(getClass().getResource("home.fxml"));
+        homeStage.show();
     }
 }

363cf9080a2c15e5364487504b90e2ce8879fcfa
NickJ20 <nickj20@student.sdu.dk>
2020-11-24 09:41:33 +0100

    edited stage structure

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,13 +2,16 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private static final String UI_FILE = "window.fxml";
     @Override
-    public void start(Stage stage_dummy) throws Exception{
-        Stage stage = FXMLLoader.load(getClass().getResource(UI_FILE));
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
     public static void main(String[] args) {

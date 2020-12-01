aac04a846fd9db1ebd63780e92a05316befb3f0e
Jeppe Holst Larsen <Holst20@live.dk>
2020-12-01 11:39:28 +0100

    Designed intro screen

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -3,11 +3,15 @@ package sample;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.effect.ColorAdjust;
 
 import java.io.IOException;
 
 public class MainController {
+
     @FXML
     private void EndGame () {
         Platform.exit();
